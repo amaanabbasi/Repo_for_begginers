5f60f0467c7858c174c96a65a227371e65be3aec
pengoox <amaanabbasi99@gmail.com>
2018-07-04 17:50:09 +0530

    homepage added

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	#
-	#
-	#
-	#
-	return  HttpResponse("hello World")
+	
+	return  render(request, 'homepage.html')
