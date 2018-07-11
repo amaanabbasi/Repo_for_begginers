8e0dec7a5dfc5f0edc9fa6297cd899af93a3856e
pengoox <amaanabbasi99@gmail.com>
2018-07-11 11:05:09 +0530

    changes

--- src/homepage/urls.py
+++ src/homepage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'homepage'
 urlpatterns = [
 	path('',views.index,name='index'),
 
--- src/homepage/views.py
+++ src/homepage/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
 	
-	return  render(request, 'homepage.html')
+	return  render(request, 'homepage/homepage.html')
