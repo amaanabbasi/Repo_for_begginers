03486938771a37758a07641325f2206d28476f31
pengoox <amaanabbasi99@gmail.com>
2018-08-14 00:21:43 +0530

    alert

--- src/insta/views.py
+++ src/insta/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
+@login_required(login_url='login/')
 def userdetail(request, *args, **kwargs):
     print(request)
     print(args)
